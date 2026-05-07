6e3bb518d0e851bc2fd73aeb812e0c4e89a93023
Andrew Peng <andrew.peng.canada@gmail.com>
2026-05-07 18:44:47 -0400

    feature: added main class

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -6,17 +6,7 @@ class FizzBuzz {
     public static void whileversion(){
         int i = 1;
         while(i < 100){
-            boolean d3 = i%3==0;
-            boolean d5 = i%5==0;
-            if(d3 && d5){
-                System.out.println("Fizz Buzz");
-            }else if(d3){
-                System.out.println("Fizz");
-            }else if(d5){
-                System.out.println("Buzz");
-            }else{
-                System.out.println(i);
-            }
+            doFizzBuzz(i);
             i++;
         }
     }
@@ -30,7 +20,7 @@ class FizzBuzz {
         whileversion();
     }
 
-    private static void doFizzBuzz(int i) {
+    public static void doFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
