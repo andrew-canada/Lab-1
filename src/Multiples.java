3e32f2b61e980e09fa43748964934a2764ab4e33
Andrew Peng <andrew.peng.canada@gmail.com>
2026-05-07 18:50:08 -0400

    added multiples class to count number of multiples of 3 or 5 less than 1000

---
+++ src/Multiples.java
@@ -0,0 +1,13 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Multiples {
+    public static void main(String[] args) {
+        int ans=0;
+        for(int i=0; i<1000; i++){
+            if (i % 3 == 0 || i % 5 == 0) {
+                ans++;
+            }
+        }
+        System.out.println(ans);
+    }
+}
